8b92808f3c38efde1bc8bb90ba9045b8b46b4a5d
Madhura Bhide <madhurabhide80@gmail.com>
2026-09-25 16:49:35 +0530

    Connect frontend to Render backend

--- ecommerce-dashboard/src/services/api.js
+++ ecommerce-dashboard/src/services/api.js
@@ -6,10 +6,35 @@
 
 // export default API;
  
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default API;
+
+
+ 
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shopsphere-backend-157b.onrender.com/api",
 });
 
 API.interceptors.request.use(
@@ -28,3 +53,4 @@ API.interceptors.request.use(
 );
 
 export default API;
+
